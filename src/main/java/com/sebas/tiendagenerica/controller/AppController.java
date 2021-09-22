8743cd726af12669e2b5a7db6cc47a9d7a940d12
Sebastian Martinez <sebasmartinez05@outlook.com>
2021-09-22 17:17:57 -0500

    first commit

---
+++ src/main/java/com/sebas/tiendagenerica/controller/AppController.java
@@ -0,0 +1,38 @@
+package com.sebas.tiendagenerica.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AppController {
+    @GetMapping({"/","/login"})
+	public String index() {
+		return "index";
+	}
+
+    @GetMapping("/menu")
+	public String menu() {
+		return "menu";
+	}
+
+    @GetMapping("/usuarios")
+	public String usuarios() {
+		return "usuarios";
+	}
+
+    @RequestMapping("/validar")
+    public String validar(HttpServletRequest request, HttpServletResponse response){
+        String usuario = request.getParameter("usuario");
+        String contrasena = request.getParameter("contrasena");
+
+        if (usuario.equals("admininicial") && contrasena.equals("123456")){
+            return "menu";
+        }else{
+            return "error";
+        }
+    }
+}
---
+++ src/test/java/com/sebas/tiendagenerica/TiendagenericaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebas.tiendagenerica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendagenericaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
